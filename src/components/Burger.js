9191c38dbd3da41265829a402d2c4893a9ed9220
vuongvgc <vuongvgc@gmail.com>
2020-12-17 22:36:25 +0700

    display in screen burger

--- src/components/Burger.js
+++ src/components/Burger.js
@@ -1,17 +1,46 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+const BurgerItem = (props) => {
+  return <div className={props.name}></div>;
+};
 class Burger extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      salad: 0,
+      cheese: 0,
+      beef: 0,
+    };
+  }
+  renderBurger = (name, index) => {
+    return <BurgerItem key={name + index} name={name} />;
+  };
+  renderBurgerList = (name, number) => {
+    let arrBurger = [];
+    for (let i = 0; i < number; i++) {
+      arrBurger.push(this.renderBurger(name, i));
+    }
+    console.log(arrBurger);
+    return arrBurger;
+  };
   render() {
+    console.log(this.props.burger);
+    const arrBurger = Object.getOwnPropertyNames(this.props.burger);
+    console.log(arrBurger);
+    let burgerList = arrBurger.map((item) => {
+      return this.renderBurgerList(item, this.props.burger[item]);
+    });
     return (
       <div className="burger__box">
         <h3 className="text-center">Cửa Hàng Burger CyberSoft</h3>
         <div className="breadTop"></div>
-        <div className="salad"></div>
-        <div className="cheese"></div>
-        <div className="beef"></div>
+        {burgerList}
         <div className="breadBottom"></div>
       </div>
     );
   }
 }
-
-export default Burger;
+const mapStateToProps = (state) => {
+  return { burger: state.BurgerReducer.burger };
+};
+export default connect(mapStateToProps)(Burger);
